db13fdb923cc006fe1976d39b4adf0666cacc51a
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-09-25 11:45:12 +0200

    Create timerconfig.c
    Timer Linking Configuration File. Developed By Mina Raouf

---
+++ timerconfig.c
@@ -0,0 +1,16 @@
+/*
+ * timerconfig.c
+ *
+ *  Created on: Sep 20, 2019
+ *      Author: Mina Raouf
+ */
+
+#include"DIO.h"
+#include "timerconfig.h"
+
+
+
+TIMER_cnfg_t TIMER_cnfg_arr [TOTAL_TIMERS] =
+{
+
+};
